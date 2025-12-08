eb3ca6b6dc5d236c2ca27bed68489f194de1e235
Abid Rehman <abidwazir994@gmail.com>
2025-12-08 19:27:03 +0500

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 function App(){
-  const title = "Welcome to KitabKor";
+  const title = "KitabKor";
+  // const book={name:'human',price:30}  //React doesnt convert object & Booleans
   return(
     <div className='App'>
       <div className='content'>
         <h1>{title}</h1>
+        
+        <h1>{'Kitabkor'}</h1>
       </div>
     </div>
   )
 }
 export default App;
+
---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = ()=> {
+    return(
+     <nav className="navbar">
+        <h1>KitabKor</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">Home</a>
+        </div>
+     </nav>
+    )
+}
+export default Navbar;
