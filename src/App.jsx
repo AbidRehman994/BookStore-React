2b248632bd0a352a84d5a71a3dfca5db7b4b417a
Abid Rehman <abidwazir994@gmail.com>
2025-12-08 02:10:15 -0800

    renamed main.jsx as index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import './App.css';
 
+function App(){
+  const title = "Welcome to KitabKor";
+  return(
+    <div className='App'>
+      <div className='content'>
+        <h1>{title}</h1>
+      </div>
+    </div>
+  )
+}
+export default App;
