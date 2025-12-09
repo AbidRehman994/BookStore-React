001605499890984f5bb8262449ce678ad8a21344
Abid Rehman <abidwazir994@gmail.com>
2025-12-09 21:54:43 +0500

    added images & cart event button

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import sapiensCover from'./assets/sapiens.webp'
 const Home = () => {
   const [books, setBooks]=useState([
     {title:'Homo Sapiens',body:'New-York Bestseller of the year',author:'Yuval Harrari',
-      price:3000, id:'1'
+      price:3000, cover:sapiensCover, id:'1'
     },
     {title:'Think Fast',body:'Guardian bestbook of the year',author:'Malcolm',
       price:350,id:'2'
@@ -28,9 +29,11 @@ const Home = () => {
         <div className="books-container">
      {books.map((books)=>(
     <div className="blog-preview" key={books.id}>
+      <img src={books.cover} alt={books.title} />
       <h2>{books.title}</h2>
       <p>{books.author}</p>
-      <p>{books.price}</p>
+      <p>{books.price}PKR</p>
+      <button onClick={()=>alert(books.title+" added to cart!")}>Add to Cart</button>
     </div>
      ))}
     </div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ()=> {
         <h1>KitabKor</h1>
         <div className="links">
             <a href="/">Home</a>
-            <a href="/create">Cart</a>
+            <a href="/cart">Cart</a>
             <a href="log-in">log-in</a>
         </div>
      </nav>
