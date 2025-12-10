dee06525fcbc5c87ca160433bbb939803067b5e9
Abid Rehman <abidwazir994@gmail.com>
2025-12-10 21:51:55 +0500

    feat: added bookpics

--- src/BookCard.jsx
+++ src/BookCard.jsx
@@ -1,11 +1,12 @@
 const BookCard = (props)=>{
     const books=props.books;
+    const title = props.title; // get title from props
     return(
         <div className="book-list">
-             <h1>BestSellers</h1>
+             <h1>{title}</h1>
         <div className="books-container">
      {books.map((books)=>(
-    <div className="blog-preview" key={books.id}>
+    <div className="book-preview" key={books.id}>
       <img src={books.cover} alt={books.title} 
       style={{width: "100%",height:"200px",overflow:"hidden"}} />
       <h2>{books.title}</h2>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,34 +1,26 @@
 import { useState } from "react";  
 import sapiensCover from'./assets/sapiens.webp';
+import thinkCover from './assets/Think.jpg';
+import humanCover from './assets/human.png';
 import BookCard from './BookCard';
 
 const Home = () => {
   const [books, setBooks]=useState([
     {title:'Homo Sapiens',body:'New-York Bestseller of the year',author:'Yuval Harrari',
-      price:3000, cover:sapiensCover, id:'1'
+      price:3000, cover:sapiensCover,category:"History", id:'1'
     },
-    {title:'Think Fast',body:'Guardian bestbook of the year',author:'Daniel',
-      price:350, cover:thinkCover, id:'3'
+    {title:'Think Fast',body:'Guardian bestbook of the year', author:'Daniel',
+      price:350, cover:thinkCover, category:"Psychology" ,id:'2'
     },
-      {title:'The Human Kind',body:'must-read',author:'Ruther',
-      price:450,id:'2'
+    {title:'The Human Kind',body:'must-read',author:'Ruther',
+      price:450,category:"History",cover:humanCover,id:'3'
     },
-     {title:'The Human Kind',body:'must-read',author:'Ruther',
-      price:450,id:'2'
-    },
-     {title:'The Human Kind',body:'must-read',author:'Ruther',
-      price:450,id:'2'
-    },
-     {title:'The Human Kind',body:'must-read',author:'Ruther',
-      price:450,id:'2'
-    },
-     {title:'The Human Kind',body:'must-read',author:'Ruther',
-      price:450,id:'2'
-    }
+    {title:'Psychology',author:'Goldman',category:"Psychology",id:'4'}
   ])
   return (
     <div className="home">
-     <BookCard books={books}/>
+     <BookCard books={books} title="BestSellers"/>
+     <BookCard books={books.filter((book)=>book.category==="Psychology")} title="Psychology Books"/>
     </div>
   );
 };
