420af4f19576296133bc57d1ca632ff57f1f932a
Abid Rehman <abidwazir994@gmail.com>
2025-12-12 13:22:54 +0500

    feat: Added function as a prop for rating

--- src/BookCard.jsx
+++ src/BookCard.jsx
@@ -1,18 +1,33 @@
-const BookCard = (props)=>{
-    const books=props.books;
-    const title = props.title; // get title from props
+const BookCard = ({books,title,onRate})=>{
+    // const books=props.books;
+    // const title = props.title; // get title from props
     return(
         <div className="book-list">
              <h1>{title}</h1>
         <div className="books-container">
      {books.map((books)=>(
     <div className="book-preview" key={books.id}>
+
       <img src={books.cover} alt={books.title} 
       style={{width: "100%",height:"200px",overflow:"hidden"}} />
+
       <h2>{books.title}</h2>
       <p>{books.author}</p>
       <p>{books.price}PKR</p>
-      <button onClick={()=>alert(books.title+" added to cart!")}>Add to Cart</button>
+       <p>Rating: {books.rating} ⭐</p>
+
+
+      {/* click event */}
+      <button onClick={()=>alert(books.title+" added to cart!")}>Add to Cart</button> 
+       <button onClick={() => onRate(book.id, book.rating + 0.5)}>
+              + Add 0.5 ★
+            </button>
+
+            {/* Decrease rating by 0.5 per click */}
+            <button onClick={() => onRate(book.id, book.rating - 0.5)}>
+              - Remove 0.5 ★
+            </button>
+      
     </div>
      ))}
     </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -7,20 +7,30 @@ import BookCard from './BookCard';
 const Home = () => {
   const [books, setBooks]=useState([
     {title:'Homo Sapiens',body:'New-York Bestseller of the year',author:'Yuval Harrari',
-      price:3000, cover:sapiensCover,category:"History", id:'1'
+      price:3000, cover:sapiensCover,category:"History", rating:4,id:'1'
     },
     {title:'Think Fast',body:'Guardian bestbook of the year', author:'Daniel',
-      price:350, cover:thinkCover, category:"Psychology" ,id:'2'
+      price:350, cover:thinkCover, category:"Psychology",rating:3.5,id:'2'
     },
     {title:'The Human Kind',body:'must-read',author:'Ruther',
       price:450,category:"History",cover:humanCover,id:'3'
     },
     {title:'Psychology',author:'Goldman',category:"Psychology",id:'4'}
   ])
+   // ⭐ Function to update rating
+  const updateRating = (id, newRating) => {
+    setBooks(prev =>
+      prev.map(book =>
+        book.id === id ? { ...book, rating: newRating } : book
+      )
+    );
+  };
+
   return (
     <div className="home">
-     <BookCard books={books} title="BestSellers"/>
+     <BookCard books={books} title="BestSellers"  onRate={updateRating}/>
      <BookCard books={books.filter((book)=>book.category==="Psychology")} title="Psychology Books"/>
+    
     </div>
   );
 };
